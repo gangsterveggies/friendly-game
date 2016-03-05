227a209fbbfa6c799d79baa09e252ba2d437371b
Pedro <gangsterveggies@gmail.com>
2016-03-05 12:16:07 +0000

    Initial commit

---
+++ client/templates/main.js
@@ -0,0 +1,11 @@
+Template.main.onRendered(function () {
+/*  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
+
+  game.state.add('Boot', BasicGame.Boot);
+  game.state.add('Preloader', BasicGame.Preloader);
+  game.state.add('MainMenu', BasicGame.MainMenu);
+  game.state.add('Game', BasicGame.Game);
+  game.state.start('Boot');*/
+  console.log("Game Started!");
+});
+
